f69d9ef7b1067cd0a20d4fbb52722508981b85d6
EmreKarahan <emre.karahan@gmail.com>
2014-08-20 00:21:08 +0300

    Some extension methods added

--- Frost.Extensions/DateTimeExtensions.cs
+++ Frost.Extensions/DateTimeExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 
 namespace Frost.Extensions
 {
@@ -27,5 +29,223 @@ namespace Frost.Extensions
                 throw new ArgumentNullException("dateTime");
             return dateTime.ToString(@"yyyy-MM-ddTHH\:mm\:ss.fffffffzzz", CultureInfo.InvariantCulture);
         }
+
+        public static string ToYearString(this DateTime dateTime)
+        {
+            if (dateTime == null)
+                throw new ArgumentNullException("dateTime");
+
+            return dateTime.ToString("yyyy");
+        }
+
+        public static string ToMonthString(this DateTime dateTime)
+        {
+            if (dateTime == null)
+                throw new ArgumentNullException("dateTime");
+
+            return dateTime.ToString("MM");
+        }
+
+        public static string ToDayString(this DateTime dateTime)
+        {
+            if (dateTime == null)
+                throw new ArgumentNullException("dateTime");
+
+            return dateTime.ToString("dd");
+        }
+
+        public static string ToSqlServerFormat(this DateTime dateTime)
+        {
+            if (dateTime == null)
+                throw new ArgumentNullException("dateTime");
+
+            return dateTime.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
+        }
+
+        public static string ToOracleSqlDate(this DateTime dateTime)
+        {
+            return String.Format("to_date('{0}','dd.mm.yyyy hh24.mi.ss')", dateTime.ToString("dd.MM.yyyy HH:mm:ss"));
+        }
+
+        public static bool IsWeekend(this DateTime value)
+        {
+            return (value.DayOfWeek == DayOfWeek.Sunday || value.DayOfWeek == DayOfWeek.Saturday);
+        }
+
+        public static int Age(this DateTime dateOfBirth)
+        {
+            if (DateTime.Today.Month < dateOfBirth.Month ||
+            DateTime.Today.Month == dateOfBirth.Month &&
+             DateTime.Today.Day < dateOfBirth.Day)
+            {
+                return DateTime.Today.Year - dateOfBirth.Year - 1;
+            }
+            else
+                return DateTime.Today.Year - dateOfBirth.Year;
+        }
+
+        public static IEnumerable<DateTime> GetDateRangeTo(this DateTime self, DateTime toDate)
+        {
+            var range = Enumerable.Range(0, new TimeSpan(toDate.Ticks - self.Ticks).Days);
+
+            return from p in range
+                   select self.Date.AddDays(p);
+        }
+
+        public static Int64 DateDiff(this DateTime startDate, String datePart, DateTime endDate)
+        {
+            Int64 dateDiffVal = 0;
+            Calendar cal = System.Threading.Thread.CurrentThread.CurrentCulture.Calendar;
+            TimeSpan ts = new TimeSpan(endDate.Ticks - startDate.Ticks);
+            switch (datePart.ToLower().Trim())
+            {
+                #region year
+                case "year":
+                case "yy":
+                case "yyyy":
+                    dateDiffVal = (Int64)(cal.GetYear(endDate) - cal.GetYear(startDate));
+                    break;
+                #endregion
+
+                #region quarter
+                case "quarter":
+                case "qq":
+                case "q":
+                    dateDiffVal = (Int64)((((cal.GetYear(endDate)
+                                        - cal.GetYear(startDate)) * 4)
+                                        + ((cal.GetMonth(endDate) - 1) / 3))
+                                        - ((cal.GetMonth(startDate) - 1) / 3));
+                    break;
+                #endregion
+
+                #region month
+                case "month":
+                case "mm":
+                case "m":
+                    dateDiffVal = (Int64)(((cal.GetYear(endDate)
+                                        - cal.GetYear(startDate)) * 12
+                                        + cal.GetMonth(endDate))
+                                        - cal.GetMonth(startDate));
+                    break;
+                #endregion
+
+                #region day
+                case "day":
+                case "d":
+                case "dd":
+                    dateDiffVal = (Int64)ts.TotalDays;
+                    break;
+                #endregion
+
+                #region week
+                case "week":
+                case "wk":
+                case "ww":
+                    dateDiffVal = (Int64)(ts.TotalDays / 7);
+                    break;
+                #endregion
+
+                #region hour
+                case "hour":
+                case "hh":
+                    dateDiffVal = (Int64)ts.TotalHours;
+                    break;
+                #endregion
+
+                #region minute
+                case "minute":
+                case "mi":
+                case "n":
+                    dateDiffVal = (Int64)ts.TotalMinutes;
+                    break;
+                #endregion
+
+                #region second
+                case "second":
+                case "ss":
+                case "s":
+                    dateDiffVal = (Int64)ts.TotalSeconds;
+                    break;
+                #endregion
+
+                #region millisecond
+                case "millisecond":
+                case "ms":
+                    dateDiffVal = (Int64)ts.TotalMilliseconds;
+                    break;
+                #endregion
+
+                default:
+                    throw new Exception(String.Format("DatePart \"{0}\" is unknown", datePart));
+            }
+            return dateDiffVal;
+        }
+
+        public static bool IsWeekday(this DayOfWeek d)
+        {
+            switch (d)
+            {
+                case DayOfWeek.Sunday:
+                case DayOfWeek.Saturday: return false;
+
+                default: return true;
+            }
+        }
+
+        public static DateTime AddWorkdays(this DateTime d, int days)
+        {
+            // start from a weekday
+            while (d.DayOfWeek.IsWeekday()) d = d.AddDays(1.0);
+            for (int i = 0; i < days; ++i)
+            {
+                d = d.AddDays(1.0);
+                while (d.DayOfWeek.IsWeekday()) d = d.AddDays(1.0);
+            }
+            return d;
+        }
+
+        public static DateTime GetLastDayOfMonth(this DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, 1).AddMonths(1).AddDays(-1);
+        }
+
+        public static long ToUnixTimestamp(this DateTime date)
+        {
+            DateTime unixEpoch = new DateTime(1970, 1, 1, 0, 0, 0);
+            TimeSpan unixTimeSpan = date - unixEpoch;
+
+            return (long)unixTimeSpan.TotalSeconds;
+        }
+
+        public static string ToRFC822DateString(this DateTime date)
+        {
+            int offset = TimeZone.CurrentTimeZone.GetUtcOffset(DateTime.Now).Hours;
+            string timeZone = "+" + offset.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0');
+            if (offset < 0)
+            {
+                int i = offset * -1;
+                timeZone = "-" + i.ToString().PadLeft(2, '0');
+            }
+            return date.ToString("ddd, dd MMM yyyy HH:mm:ss " + timeZone.PadRight(5, '0'), CultureInfo.GetCultureInfo("en-US"));
+        }
+
+        public static DateTime EndOfTheMonth(this DateTime date)
+        {
+            var endOfTheMonth = new DateTime(date.Year, date.Month, 1)
+                .AddMonths(1)
+                .AddDays(-1);
+
+            return endOfTheMonth;
+        }
+
+        public static TimeSpan TimeElapsed(this DateTime date)
+        {
+            return DateTime.Now - date;
+        }
+        public static Double ElapsedSeconds(this DateTime input)
+        {
+            return DateTime.Now.Subtract(input).TotalSeconds;
+        }
+
     }
 }
---
+++ Frost.Extensions/StringExtensions.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Frost.Extensions
+{
+    public static class StringExtensions
+    {
+        public static DateTime? ToDateTime(this string s)
+        {
+            DateTime dtr;
+            var tryDtr = DateTime.TryParse(s, out dtr);
+            return (tryDtr) ? dtr : new DateTime?();
+        }
+
+        public static bool IsNumeric(this string theValue)
+        {
+            long retNum;
+            return long.TryParse(theValue, System.Globalization.NumberStyles.Integer, System.Globalization.NumberFormatInfo.InvariantInfo, out retNum);
+        }
+
+        public static bool IsValidEmailAddress(this string s)
+        {
+            Regex regex = new Regex(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
+            return regex.IsMatch(s);
+        }
+
+        public static string Truncate(this string text, int maxLength)
+        {
+            // replaces the truncated string to a ...
+            const string suffix = "...";
+            string truncatedString = text;
+
+            if (maxLength <= 0) return truncatedString;
+            int strLength = maxLength - suffix.Length;
+
+            if (strLength <= 0) return truncatedString;
+
+            if (text == null || text.Length <= maxLength) return truncatedString;
+
+            truncatedString = text.Substring(0, strLength);
+            truncatedString = truncatedString.TrimEnd();
+            truncatedString += suffix;
+            return truncatedString;
+        }
+
+        public static string StripHtml(this string input)
+        {
+            // Will this simple expression replace all tags???
+            var tagsExpression = new Regex(@"</?.+?>");
+            return tagsExpression.Replace(input, " ");
+        }
+
+        public static string Left(this string s, int length)
+        {
+            length = Math.Max(length, 0);
+
+            if (s.Length > length)
+            {
+                return s.Substring(0, length);
+            }
+            else
+            {
+                return s;
+            }
+        }
+
+        public static string Right(this string s, int length)
+        {
+            length = Math.Max(length, 0);
+
+            if (s.Length > length)
+            {
+                return s.Substring(s.Length - length, length);
+            }
+            else
+            {
+                return s;
+            }
+        }
+
+        public static bool IsDate(this string input)
+        {
+            if (!string.IsNullOrEmpty(input))
+            {
+                DateTime dt;
+                return (DateTime.TryParse(input, out dt));
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool IsUnicode(this string value)
+        {
+            int asciiBytesCount = System.Text.Encoding.ASCII.GetByteCount(value);
+            int unicodBytesCount = System.Text.Encoding.UTF8.GetByteCount(value);
+
+            if (asciiBytesCount != unicodBytesCount)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static string Repeat(this string input, int count)
+        {
+            if (input == null)
+            {
+                return null;
+            }
+
+            var sb = new StringBuilder();
+
+            for (var repeat = 0; repeat < count; repeat++)
+            {
+                sb.Append(input);
+            }
+
+            return sb.ToString();
+        }
+
+        public static int ToInt(this string number, int defaultInt)
+        {
+            int resultNum = defaultInt;
+            try
+            {
+                if (!string.IsNullOrEmpty(number))
+                    resultNum = Convert.ToInt32(number);
+            }
+            catch
+            {
+            }
+            return resultNum;
+        }
+
+        public static decimal ToDecimal(this string value)
+        {
+            decimal number;
+
+            Decimal.TryParse(value, out number);
+
+            return number;
+        }
+
+        public static int ToInt32(this string value)
+        {
+            int number;
+
+            Int32.TryParse(value, out number);
+
+            return number;
+        }
+
+        public static string Strip(this string data, string textToStrip)
+        {
+            var stripText = data;
+
+            if (string.IsNullOrEmpty(data)) return stripText;
+
+            try
+            {
+                stripText = Regex.Replace(data, textToStrip, string.Empty);
+            }
+            catch
+            {
+            }
+            return stripText;
+        }
+
+        public static string Strip(this string data, string textToStrip, string textToReplace)
+        {
+            var stripText = data;
+
+            if (string.IsNullOrEmpty(data)) return stripText;
+
+            try
+            {
+                stripText = Regex.Replace(data, textToStrip, textToReplace);
+            }
+            catch
+            {
+            }
+            return stripText;
+        }
+    }
+}
