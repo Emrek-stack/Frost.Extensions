166a2bc6f781f072196aed00e4d9c9eac4ef2295
EmreKarahan <emre.karahan@gmail.com>
2014-08-19 23:40:03 +0300

    Object Extensions added

---
+++ Frost.Extensions/ObjectExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Frost.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static byte[] ToByteArray(this object obj)
+        {
+            if (obj == null)
+                throw new ArgumentNullException("obj");
+
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream();
+            bf.Serialize(ms, obj);
+            return ms.ToArray();
+        }
+
+        public static T ToObject<T>(this byte[] bytes)
+        {
+            if (bytes == null)
+                throw new ArgumentNullException("bytes");
+
+            MemoryStream memStream = new MemoryStream();
+            BinaryFormatter binForm = new BinaryFormatter();
+            memStream.Write(bytes, 0, bytes.Length);
+            memStream.Seek(0, SeekOrigin.Begin);
+            object obj = binForm.Deserialize(memStream);
+            return (T)obj;
+        }
+    }
+}
