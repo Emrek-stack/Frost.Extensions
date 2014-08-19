4c85e401dd4d091f6e62afd5bec1e4e70cbd85a5
EmreKarahan <emre.karahan@gmail.com>
2014-08-19 23:50:29 +0300

    DateTime Extenions added

---
+++ Frost.Extensions/DateTimeExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Frost.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime ChangeTime(this DateTime dateTime, int hours, int minutes, int seconds, int milliseconds)
+        {
+            if (dateTime == null)
+                throw new ArgumentNullException("dateTime");
+
+            return new DateTime(
+                dateTime.Year,
+                dateTime.Month,
+                dateTime.Day,
+                hours,
+                minutes,
+                seconds,
+                milliseconds,
+                dateTime.Kind);
+        }
+    }
+}
